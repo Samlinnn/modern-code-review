25b32e18fc933c78bd2439168d94e6b5da868a87
duiwoeryan <144334452+duiwoeryan@users.noreply.github.com>
2023-09-11 21:14:58 +0800

    Update mcr.cpp
    fix an efficiency problem

--- mcr.cpp
+++ mcr.cpp
@@ -1,15 +1,16 @@
 bool isWin(char game[3][3]){
 	bool win = false;
-	if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true;
-	if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true;
+	//row
+	if ( game[0][0] != 0 && game[0][0] == game[0][1] && game[0][1] == game[0][2] ) win = true;
+	if ( game[1][0] != 0 && game[1][0] == game[1][1] && game[1][1] == game[1][2] ) win = true;
+	if ( game[2][0] != 0 && game[2][0] == game[2][1] && game[2][1] == game[2][2] ) win = true;
 	// column
-	if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) win = true;
-	if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
+	if ( game[0][0] != 0 && game[0][0] == game[1][0] && game[1][0] == game[2][0] ) win = true;
+	if ( game[0][1] != 0 && game[0][1] == game[1][1] && game[1][1] == game[2][1] ) win = true;
+	if ( game[0][2] != 0 && game[0][2] == game[1][2] && game[1][2] == game[2][2] ) win = true;
 	// diagonal
-	if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-	if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
+	if ( game[0][0] != 0 && game[0][0] == game[1][1] && game[1][1] == game[2][2] ) win = true;
+	if ( game[0][2] != 0 && game[0][2] == game[1][1] && game[1][1] == game[2][0] ) win = true;
 	return win;
 }
 
